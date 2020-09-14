e25ff2608ba8a179f9cad92912b47733e1673120
Rias <hey@rias.be>
2020-09-14 20:33:05 +0200

    woops

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ let mix = require("laravel-mix");
 mix
     .js("resources/js/button-box.js", "dist/js")
     .postCss("resources/css/button-box.css", "dist/css")
+    .copy("resources/fonts", "dist/fonts")
     .options({
         processCssUrls: false
     });
